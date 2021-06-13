cf7ba46df6d980bb4d372be117c79158fbfc4ef7
Jorge Sandoval <jorge.sandoval98@gmail.com>
2021-06-12 23:34:51 -0400

    fix i18n routes

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,17 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
+import en from './assets/locales/en/translation.json';
+import es from './assets/locales/es/translation.json';
+
+const resources = {
+  en: {
+    translation: en
+  },
+  es: {
+    translation: es
+  }
+};
 
 i18n
   .use(Backend)
@@ -8,6 +19,7 @@ i18n
   .init({
     lng: 'es',
     fallbackLng: 'en',
+    resources,
     interpolation: {
       escapeValue: false,
     }
